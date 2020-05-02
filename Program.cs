fe551e4f8e4c35868708e9b0a12bcb9ef9a25397
Wolfman13 <34652519+Wolfman13@users.noreply.github.com>
2020-05-03 00:34:38 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    private static void WriteToFile(List<string> list)
+    {
+        string outputPath = Environment.CurrentDirectory.ToString() + "\\Words.txt";
+        File.AppendAllLines(outputPath, list);
+    }
+
+    public static void Main()
+    {
+        int currentFile = 1;
+        int wordCounter = 0;
+        string inputFile = Environment.CurrentDirectory.ToString() + $"\\Files\\File{currentFile}.txt";
+
+        while (File.Exists(inputFile))
+        {
+            List<string> list = new List<string>();
+
+            Console.WriteLine($"Starting File{currentFile}.txt");
+
+            using (StreamReader reader = new StreamReader(inputFile))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    list.Add(reader.ReadLine());
+
+                    if (list.Count >= 10000000)
+                    {
+                        Console.WriteLine($"Emergency write out. Word Count: {passwordCounter}");
+                        WriteToFile(list);
+
+                        list = new List<string>();
+                    }
+
+                    wordCounter++;
+                }
+            }
+
+            Console.WriteLine($"Writing words to file. Password Count: {wordCounter}");
+            WriteToFile(list);
+
+            Console.WriteLine($"Finished File{currentFile}.txt");
+
+            currentFile++;
+            inputFile = Environment.CurrentDirectory.ToString() + $"\\Files\\File{currentFile}.txt";
+        }
+
+        Console.WriteLine("Press any key to continue...");
+        _ = Console.ReadKey();
+    }
+}
